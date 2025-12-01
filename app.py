a7f47a3c66f361c749f23ac89e3a1159e35f7194
SandeepKata <ksandeepchandu@gmail.com>
2025-12-02 02:20:52 +0530

    Updated welcome message

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello from CI/CD Flask app!</h1><p>Deployed via GitHub Actions → Docker → EC2.</p>"
+    return "<h1>Hello from UPDATED CI/CD Flask app!</h1><p>Auto deployment working!</p>"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
